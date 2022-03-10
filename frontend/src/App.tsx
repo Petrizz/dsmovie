940113cf541fc70b025555ed94fbb3e4c09d1464
Petrizz <pedrogonzagamendesoficial@gmail.com>
2022-03-10 14:49:20 -0300

    Rotas prontas
    Fizemos  uma importação do React Router DOM adicionando os elementos necessarios em app.tsx e criandado as pastas e arquivos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
-import Navbar from "./componentes/Navbar";
-
+import {
+  BrowserRouter,
+  Routes, //esses componentes que servem para confgurar as rotas
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "componentes/Navbar";
 
 function App() {
+  //<BrowserRouter>inicia a configuração das rotas
+  //<Routes> ele configura rota por rota
+  //Route manipula os aquivos que vão ser abertos e com quais atributos são necessario pra isso ex /form tem outro route com path para passa o id
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes> ele configura rota por rota
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

6757908c23fa743ada69c654cd7b9f0170b64f10
Petrizz <pedrogonzagamendesoficial@gmail.com>
2022-03-10 17:21:32 -0300

    Criamos o MovieCard
    MovieCard é a imagem do filme e sua avaliação, MovieCard é composto de sub componentes que vão sendo montados aos poucos

--- frontend/src/componentes/Navbar/index.tsx
+++ frontend/src/componentes/Navbar/index.tsx
@@ -12,7 +12,7 @@ function Navbar() {
             <a href="https://github.com/Petrizz" target="_blank" rel="noreferrer">
                 <div className="dsmovie-contact-container">
                     <GithubIcon />
-                    <p className="dsmovie-contact-link">/devsuperior</p>
+                    <p className="dsmovie-contact-link">/Petrizz</p>
                 </div>
             </a>
         </div>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,43 @@
+import MovieCard from "componentes/MovieCard";
+
 import Pagination from "componentes/Pagination";
 
-function Listing(){
-    return(
-        <Pagination />
+function Listing() {
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sn-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                   
+                </div>
+            </div>
+
+        </>
     );
 }
 export default Listing;
